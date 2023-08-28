3a82b2a66d7a6e7df808356282cb85b35173350c
AfikPeretz <afikperetz5235@gmail.com>
2023-08-28 10:03:12 +0300

    HandleJson work with s3 instead of local json

--- handle_json.py
+++ handle_json.py
@@ -1,26 +1,38 @@
 import json
 import boto3
+
+
 class HandleJson:
 
-    def __init__(self, filename='data.json'):
-        self.filename = filename
+    def __init__(self, bucket_name='json-for-mehir-lamishtaken', object_key='data/data.json'):
+        self.bucket_name = bucket_name
+        self.object_key = object_key
+        self.s3 = boto3.client('s3')
 
     def write_data_to_json(self, data):
-        with open(self.filename, 'w', encoding="utf-8") as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=4)
+        try:
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=self.object_key)
+            current_data = json.loads(response['Body'].read().decode('utf-8'))
+        except Exception:
+            current_data = {}
+        current_data.update(data)
+        updated_json = json.dumps(current_data)
+        self.s3.put_object(Body=updated_json, Bucket=self.bucket_name, Key=self.object_key)
 
     def read_data_from_json(self):
         try:
-            with open(self.filename, 'r', encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                return data
-        except FileNotFoundError:
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=self.object_key)
+            data = json.loads(response['Body'].read().decode('utf-8'))
+            return data
+        except Exception as e:
+            print(f"Error reading data from S3: {e}")
             return {}
 
     def empty_json_file(self):
         try:
-            with open(self.filename, 'w') as file:
-                file.truncate(0)
-            print("File 'data.json' emptied successfully")
+            empty_data = {}
+            empty_json = json.dumps(empty_data)
+            self.s3.put_object(Body=empty_json, Bucket=self.bucket_name, Key=self.object_key)
+            print("File emptied successfully")
         except Exception as e:
-            print(f"Error emptying file 'data.json': {e}")
+            print(f"Error emptying file: {e}")
